65656123cfb3edf37f1757233ec5b8a65f864eb2
Musaibdeshmukh <musaibdeshmukh7222@gmail.com>
2025-06-21 14:56:59 +0530

    Added Ch02.js

---
+++ Ch02.js
@@ -0,0 +1,17 @@
+// VAR, LET & CONST IN JAVASCRIPT //
+
+
+// var a = 45;
+// var a = "p"
+let b = "Harry";
+const author = "Harry"
+// author = 5 // Throws an error because constant cannot be changed
+// b = 4
+const harry = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'                  // Block scope variable will print first //
+  console.log(b)
+}
+console.log(b)
