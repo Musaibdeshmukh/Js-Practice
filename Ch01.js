e0f48dc86ae45ed872ed362fb58520dcbbc75670
Musaibdeshmukh <musaibdeshmukh7222@gmail.com>
2025-06-21 12:26:40 +0530

    Added Ch01.js

---
+++ Ch01.js
@@ -0,0 +1,13 @@
+// VARIABLES IN JAVACSCRIPT
+
+// Variables are containers for storing data values
+// In JavaScript, there are three ways to declare variables: var, let, and const
+
+
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+var a = 67 // a contains 67
+console.log(a)
+a = "harry"
+console.log(a)
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7
