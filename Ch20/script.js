e8ec3a93de6976578e2c1575254aafb9945512a3
Musaibdeshmukh <musaibdeshmukh7222@gmail.com>
2025-07-25 17:54:04 +0530

    Added Ch20

---
+++ Ch20/script.js
@@ -0,0 +1,7 @@
+// SIBLINGS AND PARENTS
+
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
