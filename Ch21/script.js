48e4166f2d4a53ec99f64ebc7fd58c355b942d7a
Musaibdeshmukh <musaibdeshmukh7222@gmail.com>
2025-07-25 18:01:44 +0530

    Added Ch21

---
+++ Ch21/script.js
@@ -0,0 +1,9 @@
+// ELEMENT ONLY NAVIGATION
+
+const changeBgRed = () => {
+  document.body.firstElementChild.style.background = "red"
+}
+
+let b = document.body
+console.log("First child of b is: ", b.firstChild)
+console.log("First Element child of b is: ", b.firstElementChild)
