b3fc41cbdfee05491dc7059435bced3e6522701a
Musaibdeshmukh <musaibdeshmukh7222@gmail.com>
2025-07-28 13:14:21 +0530

    Added Ch59

---
+++ Ch59/script.js
@@ -0,0 +1,36 @@
+// GETTERS & SETTERS IN JAVASCRIPT
+
+
+class Animal {
+  constructor(name) {
+    this._name = name
+  }
+  fly() {
+    console.log("I am flying")
+  }
+  get name() {
+    return this._name
+  }
+
+  set name(newName) {
+    this._name = newName
+  }
+
+}
+
+class Rabbit extends Animal {
+  eatCarrot() {
+    console.log("Eating carrot")
+  }
+}
+
+let a = new Rabbit("Bruno")
+a.fly()
+console.log(a.name)
+a.name = "Jack"
+console.log(a.name)
+let c = 56
+
+console.log(a instanceof Animal)
+console.log(a instanceof Rabbit)
+console.log(c instanceof Animal)
